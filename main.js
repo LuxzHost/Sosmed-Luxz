9eb5204e310f8375c47e5a8a40f6252343e08c1e
Luxz - Official <luzfimax@gmail.com>
2025-03-07 05:35:50 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const audio = document.getElementById('background-audio');
+
+// Memutar audio saat halaman dimuat
+window.onload = function() {
+  audio.play();
+}
+
+// Memastikan audio terus berjalan di latar belakang
+audio.addEventListener('ended', function() {
+  audio.play();
+});
+
